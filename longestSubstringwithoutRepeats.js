e8072d1c0d5ede66cdf13b1d8a1ea8b07ebbf82e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-05-22 17:35:09 -0700

    Create longestSubstringwithoutRepeats.js

---
+++ longestSubstringwithoutRepeats.js
@@ -0,0 +1,26 @@
+// Given a string, return the length of the longest substring without
+// repeating characters.
+// --- Example
+// lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
+// lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
+
+function lengthOfLongestSubstring(s) {
+  let hashMap = {};
+  let start = 0;
+  let maxVar = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let currentChar = s[i];
+
+    if (hashMap[currentChar] >= start) {
+      start = hashMap[currentChar] + 1;
+    }
+
+    hashMap[currentChar] = i;
+
+    maxVar = Math.max(maxVar, i - start + 1);
+  }
+  return maxVar;
+}
+
+console.log(lengthOfLongestSubstring("welcome"))
